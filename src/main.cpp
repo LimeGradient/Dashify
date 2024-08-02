c58507acfc38aad7a7ef9ba3fcd29062a4a1ecd2
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-08-02 14:50:03 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+
+		return true;
+	}
+
+	void onMyButton(CCObject*) {
+		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+	}
+};
